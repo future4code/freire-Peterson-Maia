1b88fe2c38e81ae8a5a006f6ced5e211dcbdf285
petersonmaia <peterson.maia20@gmail.com>
2022-07-06 19:45:46 -0300

    Iniciando Projeto Astromatch

---
+++ modulo3/projeto-astromatch/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function App() {
+  const [curtida, setCurtida] = useState();
+  const [matches, setMatches] = useState();
+
+
+
+  return (
+
+    <div>
+    
+    </div>
+  );
+}
+
