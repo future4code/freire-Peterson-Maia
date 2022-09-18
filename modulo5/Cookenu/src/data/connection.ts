d826abca84b5f7ef5c8457d46ea40cc090fdceaf
petersonmaia <peterson.maia20@gmail.com>
2022-09-18 18:45:57 -0300

    cookenu final

--- modulo5/Cookenu/src/data/connection.ts
+++ modulo5/Cookenu/src/data/connection.ts
@@ -1,16 +1,26 @@
-import knex from "knex";
+import knex, { Knex } from "knex";
 import dotenv from "dotenv";
 
 
 dotenv.config();
 
-export const connection = knex({
-	client: "mysql",
-	connection: {
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+export abstract class BaseDataBase{
+
+  private static connection:Knex | null = null
+
+  protected getConnetion():Knex{
+      if(!BaseDataBase.connection){
+          BaseDataBase.connection = knex({
+              client:"mysql",
+              connection:{
+                  host:process.env.DB_HOST,
+                  user:process.env.DB_USER,
+                  password:process.env.DB_PASSWORD,
+                  database:process.env.DB_DATABASE 
+              }
+          })
+      }
+
+      return BaseDataBase.connection
   }
-});
+}
