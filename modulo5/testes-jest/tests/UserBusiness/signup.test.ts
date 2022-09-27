880519f2c9db2338e43ada44c5123ee5a57b2bfe
petersonmaia <peterson.maia20@gmail.com>
2022-09-26 23:46:38 -0300

    testes-jest

---
+++ modulo5/testes-jest/tests/UserBusiness/signup.test.ts
@@ -0,0 +1,28 @@
+import { UserBusiness } from "../../src/business/UserBusiness"
+import { ISignupInputDTO } from "../../src/models/User"
+import { AuthenticatorMock } from "../mocks/services/AuthenticatorMock"
+import { HashManagerMock } from "../mocks/services/HashManagerMock"
+import { IdGeneratorMock } from "../mocks/services/IdGeneratorMock"
+import { UserDatabaseMock } from "../mocks/UserDatabaseMock"
+
+describe("Testando UserBusiness", () => {
+    const userBusiness = new UserBusiness(
+        new UserDatabaseMock(),
+        new IdGeneratorMock(),
+        new HashManagerMock(),
+        new AuthenticatorMock()
+    )
+
+    test("signup bem sucedido", async () => {
+        const input: ISignupInputDTO = {
+            name: "peterson123",
+            email: "peterson123@gmail.com",
+            password: "peterson123"
+        }
+
+        const response = await userBusiness.signup(input)
+
+        expect(response.message).toEqual("Cadastro realizado com sucesso")
+        expect(response.token).toEqual("token-mock")
+    })
+})
